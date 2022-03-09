069268e61c240745316bdf85b9ca226c32793171
daniellfoncho <dfalfonsos@unal.edu.co>
2022-03-09 15:24:33 -0500

    new project

---
+++ client/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from "./Transactions.json";
+
+export const contractABI = abi.abi;
+export const contractAddress = "0xBB3d13A58AdBb64043A5096A525E712D4FaE4276";
---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,14 @@
+// glide extra judge dismiss ugly soccer popular river achieve juice gym aim
+//
+
+require ("@nomiclabs/hardhat-waffle");
+
+module.exports = {
+  solidity: "0.8.0",
+  networks : {
+    ropsten: {
+      url : "https://eth-ropsten.alchemyapi.io/v2/ysNcHNgVZTesc8yUe_uctARIXpNVlVmD",
+      accounts : ["b309b3efbf3b8e0426d3175b5444be08e64529239ce552ecb444cd747cf8b513"]
+    }
+  }
+}
